17a8c49291e21a3b8b6a3ce25aa2a4d60547e5d3
AI Studio <noreply+aistudio@leapwork.local>
2026-06-17 12:36:24 +0530

    Update src/tests/leapandleash/TS-1.spec.ts from AI Studio

--- src/tests/leapandleash/TS-1.spec.ts
+++ src/tests/leapandleash/TS-1.spec.ts
@@ -1,6 +1,6 @@
 import { leapwork } from "./leapwork";
  
-// comment added new
+// comment added again
   await page.goto('https://demoapps.leapwork.ai/');
   const page1Promise = page.waitForEvent('popup');
   await page.getByLabel('Retail demo resources').getByRole('link', { name: 'Open demo' }).click();
