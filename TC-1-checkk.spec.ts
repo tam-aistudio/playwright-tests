9227608620bbc776e9540aba8a445c72e5c18808
Leapwork Play <noreply+leapwork-play@leapwork.local>
2026-08-06 22:03:49 +0530

    commit

---
+++ TC-1-checkk.spec.ts
@@ -0,0 +1,11 @@
+import { leapwork } from "./leapwork";
+
+leapwork.configuration({
+  timeoutMs: Number(
+    leapwork.team.settings.get("timeoutMs")
+    ?? leapwork.workspace.settings.get("timeoutMs")
+  ) || 5000,
+  enableSelfHeal:
+    (leapwork.team.settings.get("enableSelfHeal")
+      ?? leapwork.workspace.settings.get("enableSelfHeal")) !== "false",
+});
